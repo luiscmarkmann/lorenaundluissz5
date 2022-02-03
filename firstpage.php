21804221300a49af6bb692f316b45cabbe00ca00
luiscmarkmann <98481056+luiscmarkmann@users.noreply.github.com>
2022-02-03 10:24:17 +0100

    Add files via upload

---
+++ firstpage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="firstpage.css">
+    <title>Spiel des Lebens - Lorena und Luis</title>
+    <link rel = "icon" href = "icon.png" type = "image/x-icon">
+</head>
+<body>
+    <section class="showcase">
+        <div class="video-container">
+            <video src="introvid.mp4" autoplay muted loop></video>
+        </div>
+        <div class="content">
+            <h1>Conways Spiel des Lebens</h1>
+            <h3>Lorena und Luis - SZ 5</h3>
+            <a href="/game/beispiel.php"><button class="btn">Beispiel</button></a>
+            <a href="https://playgameoflife.com/"><button class="btn">PLAY!</button></a>
+        </div>
+</body>
+</html>
